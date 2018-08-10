f554912ccf7c258c5dbf37a1c934969f28317e4f
sangshu329 <sangshu.sun@163.com>
2018-08-10 14:45:18 +0800

    first commit

---
+++ Snlog.php
@@ -0,0 +1,177 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ADMIN
+ * Date: 2018-08-06
+ * Time: 13:56
+ */
+Class Snlog
+{
+    private $seaslog_tag = 'appLog';
+    private $path = '';
+    private $debugInfo;
+    private $seaslogEable = 0;
+    private $recodeMode = 0;  // 是否启用双模式记录日志
+
+    public function __construct($tag = '', $path = '', $mode = 0, $islocal_log = 0)
+    {
+        is_string($tag) && !empty($tag) ? $this->seaslog_tag = $tag : '';
+        is_dir($path) && !empty($path) ? $this->path = $path : '';
+        $mode == 'double' || $mode === 1 ? $this->recodeMode = $mode : '';
+        $this->seaslogEable = $this->checkSeaslog();
+        $backtrace = debug_backtrace();
+        $this->debugInfo = array_pop($backtrace);
+    }
+
+    /**
+     * @日志记录主方法
+     *
+     * @param string $key
+     * @param string $value
+     * @param int $isurl
+     * @param string $tag
+     * @param int $is_local
+     */
+    public function resnLog($key = '', $value = '', $isurl = 0, $tag = 'debug', $is_local = 0)
+    {
+        if ($isurl && $this->getRuntime() != 'cli') {
+            if ($this->seaslogEable) {
+                $this->seaslogLog($key, $value, 1);
+                if ($this->recodeMode) {
+                    $this->localLog($key, $value, 1);
+                }
+            } else {
+                $this->localLog($key, $value, 1);
+            }
+        } else {
+            if ($this->seaslogEable) {
+                $this->seaslogLog($key, $value);
+                if ($this->recodeMode) {
+                    $this->localLog($key, $value);
+                }
+            } else {
+                $this->localLog($key, $value);
+            }
+        }
+    }
+
+
+    private function checkSeaslog()
+    {
+        if (class_exists('SeasLog')) {
+            return true;
+        }
+        return false;
+    }
+
+    public function localLog($key, $value, $isurl = '', $fun = 0)
+    {
+        $filePath = $this->debugInfo['file'];
+        $path = str_replace('\\', '/', str_replace(basename($filePath), '', $filePath));
+
+        if (is_dir($path) && $this->isWritable($path)) {
+        } else {
+            if ($this->getOS() == 'Windows') {
+                $path = './';
+            } else {
+                $path = '/tmp/';
+            }
+//            echo '日志指定路径不可写，'
+        }
+        if (!$fun) {
+            $user_fun = 'serialize';
+        } else {
+            $user_fun = 'json_encode';
+        }
+        if (is_array($value) || is_object($value)) {
+            $value = call_user_func($user_fun, $value);
+        }
+        if (!is_dir($path . 'logs')) @mkdir($path . 'logs', 0777, true);
+
+        if ($isurl) {
+            $url = $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'] . '?' . $_SERVER['QUERY_STRING'];
+
+            $log_data = implode(PHP_EOL, array(
+                '#####',
+                '[' . date('Y-m-d H:i:s') . '] ',
+                'post_data-    ' . stripslashes(json_encode($_POST, JSON_UNESCAPED_UNICODE)),
+                'get_data-   ' . json_encode($_GET, JSON_UNESCAPED_UNICODE),
+                'request_url-  ' . $url,
+                '#####',
+            ));
+        } else {
+            $log_data = '[' . date('Y-m-d H:i:s') . ']  ' . $key . '->' . $value . "\r\n";
+        }
+        file_put_contents($path . 'logs/' . $this->seaslog_tag . date('Ymd') . '.log', $log_data, FILE_APPEND | LOCK_EX);
+    }
+
+    public function seaslogLog($key = '', $data = '', $isurl = '')
+    {
+        SeasLog::setLogger($this->seaslog_tag);
+        if ($isurl) {
+            $url = $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'] . '?' . $_SERVER['QUERY_STRING'];
+            SeasLog::info('request_url' . "\t" .
+                implode("\t", array(
+                    'post_data-    ' . stripslashes(json_encode($_POST, JSON_UNESCAPED_UNICODE)),
+                    'get_data-   ' . json_encode($_GET, JSON_UNESCAPED_UNICODE),
+                    'request_url-  ' . $url,
+                )));
+        } else {
+            SeasLog::info('log_data' . "\t" .
+                implode("\t", array(
+                    "$key  => " . stripslashes(json_encode($data, JSON_UNESCAPED_UNICODE)),
+                ))
+            );
+        }
+    }
+
+    public static function isWritable($filename)
+    {
+        if (preg_match('/\/$/', $filename)) {
+            $tmp_file = sprintf('%s%s.tmp', $filename, uniqid(mt_rand()));
+            return self::isWritable($tmp_file);
+        }
+        if (file_exists($filename)) {
+            $fp = @fopen($filename, 'r+');
+            if ($fp) {
+                fclose($fp);
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            $fp = @fopen($filename, 'w');
+            if ($fp) {
+                fclose($fp);
+                unlink($filename);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public static function getOS()
+    {
+        if (PATH_SEPARATOR == ':') {
+            return 'Linux';
+        } else {
+            return 'Windows';
+        }
+    }
+
+    public static function getRuntime()
+    {
+        $runMode = php_sapi_name();
+        $isHaveQuery = isset($_SERVER['QUERY_STRING']);
+        switch ($runMode) {
+            case 'fpm-fcgi':
+                return $isHaveQuery ? 'web' : 'cli';
+            case 'apache2handler':
+                return $isHaveQuery ? 'web' : 'cli';
+            case 'cli':
+                return 'cli';
+        }
+    }
+}
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ADMIN
+ * Date: 2018-08-10
+ * Time: 09:39
+ */
+require 'Snlog.php';
+$log = new Snlog('','');
+$log->resnLog('test','value');
